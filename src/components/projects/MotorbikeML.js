67f6ec73d096410aedd33f3a8671e452e458593b
Kees Untamed <keest@protonmail.com>
2022-08-14 21:10:42 +1200

    Update MotorbikeML.js

--- src/components/projects/MotorbikeML.js
+++ src/components/projects/MotorbikeML.js
@@ -24,9 +24,10 @@ export default function MotorbikeML() {
                             <ItchDownload src='1187235' download='https://soggykees.itch.io/machine-learning-motorbike-riding' title='Birthday Gift by Kees Tucker' />
                             <ProjectTextCard>
                                 <h2>About</h2>
-                                <p>During Lockdown (Coronavirus pandemic) I was missing my motorbike so I decided to start a Motorbike physics sim, I started off with balance and used Machine Learning, this using the PPO algorithm. I also rigged up a character to ride the bike using Inverse Kinematics. The next steps are to model real-world engine performance, tire physics lock in my bike physics (wheelies etc).</p>
+                                <p>During Lockdown (Coronavirus pandemic) I was missing my motorbike so I decided to start a Motorbike physics sim, I started off with balance and used Machine Learning, this using the PPO algorithm. I also rigged up a character to ride the bike using Inverse Kinematics. The next steps are to model real-world engine performance (partially complete, see below), tire physics lock in my bike physics (wheelies etc).</p>
                                 <p>I was having a few issues - the algorithm was hitting a plateau and couldn't improve further. After a few tweaks, including trying to streamline and make the movement more organic, I ended up with a very efficient brain but it was the opposite of organic (see below). It switches as fast as possible from max to min to balance, somewhat exploiting the system. Further work is required to get his nice and smooth.</p>
                             </ProjectTextCard>
+                            <ProjectImageCard src="/images/mlmotorbike/bmep.png" title='BMEP pressure curve calculation. Used to calculate horsepower. This required an insane amount of physics research and testing.'/>
                             <ProjectVideoCard src="/videos/mlmotorbike2.mp4"/>
                         </Col>
                     </Row>
